139473ea95dbdeb0fa6cfbd48feebf1214077bc3
Gherman Adrian <adrian.gherman@isa.utm.md>
2021-10-07 20:12:43 +0300

    FinalCode

--- src/lab2/task1/main.java
+++ src/lab2/task1/main.java
@@ -5,7 +5,10 @@ public class main {
 
     public static void main(String[] args) {
         Box box = new Box(2);
-        box.area();
-        box.volume();
+        Box box1 = new Box(2);
+        Box box2 = new Box(2,3,4);
+
+        box2.area();
+        box2.volume();
     }
 }
