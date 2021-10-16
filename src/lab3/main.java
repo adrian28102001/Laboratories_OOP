de82b7349602447b9e9d395b671e45b57af5e825
Gherman Adrian <adrian.gherman@isa.utm.md>
2021-10-16 15:34:55 +0300

    Lab3-final

--- src/lab3/main.java
+++ src/lab3/main.java
@@ -3,16 +3,19 @@ package lab3;
 public class main {
     public static void main(String[] args) {
         String MyText = "Find out exactly how many sentences are in your text content using this online sentence counter! " +
-                "This sentence counting tool will also give you basic information on the number of words and characters in your text." +
+                "This sentence counting tool will also give you basic information on the number of words and characters in your text. " +
                 "This tool will automatically figure out the number of sentences, words, and characters that you have in most any " +
                 "type of text content. The text information you want to analyze can be many formats. This sentence counter can " +
                 "handle anything from a single string of text or to very long content composed of numerous text paragraphs separated " +
                 "by multiple line breaks.";
         textManipulation text = new textManipulation();
         text.setText(MyText);
+        text.countWords();
         text.countSentences();
         text.countLetters();
         text.countVowelsConsonants();
-        text.countTopWordsUsingJava();
+        text.longestWordFunction();
+        //text.longestWord(); this is using Java8
+        text.countTopWords();
     }
 }
--- src/lab3/textManipulation.java
+++ src/lab3/textManipulation.java
@@ -1,14 +1,10 @@
 package lab3;
 
 import java.util.*;
-
 import java.util.stream.Collectors;
 
 public class textManipulation {
     private String text = " ";
-    private int counterVowel;
-    private int counterConsonants;
-    private int counterSentences;
 
 
     //setter of a text
@@ -16,13 +12,28 @@ public class textManipulation {
         this.text = text;
     }
 
+    public String getText() {
+        return text;
+    }
+
     //function to count sentences in a text
     public void countSentences() {
+        int counterSentences = 0;
         for (int i = 0; i < text.length(); i++) {
             if (text.charAt(i) == '.' || text.charAt(i) == '?' || text.charAt(i) == '!') //checks for ending of a sentence
                 counterSentences++;
         }
         System.out.println("The number of sentences " + counterSentences);
+    }
+
+    public void countWords() {
+        int wordCount = 1; //1 because each text will end in an . or ! which is not an white space as we ask. For example "Ana has apples!" 2 whitespaces, 3 words
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == ' ') {
+                wordCount++;
+            }
+        }
+        System.out.println("In your text, there are: " + wordCount + " words");
     }
 
     //function to count letters in a text
@@ -33,26 +44,30 @@ public class textManipulation {
     }
 
     public void countVowelsConsonants() {
+        int counterVowel = 0;
+        int counterConsonants = 0;
         String str = text.toLowerCase();
-
-        for (int i = 0; i < str.length(); i++) {
+        String temporary = str.replaceAll("[^a-zA-Z0-9]", ""); //take out every symbol from the text
+        for (int i = 0; i < temporary.length(); i++) {
             //Checks whether a character is a vowel
-            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+            if (temporary.charAt(i) == 'a' || temporary.charAt(i) == 'e' || temporary.charAt(i) == 'i' || temporary.charAt(i) == 'o' || temporary.charAt(i) == 'u') {
                 //Increments the vowel counter
                 counterVowel++;
             }
             //Checks whether a character is a consonant
-            else if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
-                //Increments the consonant counter
+            else {
+
                 counterConsonants++;
             }
         }
         System.out.println("Number of vowels: " + counterVowel);
         System.out.println("Number of consonants: " + counterConsonants);
     }
+
     //this is the easiest method to implement it, by using power of Java libraries
     // but there is a similar function in class wordsCounterInAText
-    public void countTopWordsUsingJava() {
+    public void countTopWords() {
+
         String strArray[] = text.split(" ");
         for (int i = 0; i < strArray.length; i++) {
             strArray[i] = strArray[i].replaceAll("[^\\w]", "");
@@ -66,6 +81,21 @@ public class textManipulation {
         System.out.println(result);
     }
 
+    public void longestWord() { //this function uses JavaPower
+        String longest = Arrays.stream(text.split(" ")).max(Comparator.comparingInt(String::length)).orElse(null); //
+        System.out.println("Longest text word in this text is: " + longest);
+    }
+
+    public void longestWordFunction() { //this function uses my knowledge
+        String[] word = text.split(" ");
+        String longest = " ";
+
+        for (int i = 0; i < word.length; i++) {
+            if (word[i].length() > longest.length())
+                longest = word[i];
+        }
+        System.out.println("Longest text word in this text is: " + longest);
+    }
 
 }
 
