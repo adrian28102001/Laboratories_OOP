92145bda7bb2a7d9c6d366788b4cac8611529d56
Gherman Adrian <adrian.gherman@isa.utm.md>
2021-11-10 12:13:44 +0200

    lab5

--- src/lab5/Task1/Main.java
+++ src/lab5/Task1/Main.java
@@ -3,14 +3,23 @@ package lab5.Task1;
 public class Main {
     public static void main(String[] args) {
         A a = new A();
+        System.out.println("---------");
         B b = new B();
+        System.out.println("---------");
         C c = new C();
+        System.out.println("---------");
         D d = new D();
+        System.out.println("---------");
         E e = new E();
+        System.out.println("---------");
         F f = new F();
+        System.out.println("---------");
         G g = new G();
+        System.out.println("---------");
         H h = new H();
+        System.out.println("---------");
         I i = new I();
+        System.out.println("---------");
         J j = new J();
     }
 }
